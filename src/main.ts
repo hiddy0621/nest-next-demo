622089fd6208e948dda5410e7aceb7dcce922ee6
hidenao-otake <h0i6d2e1@gmail.com>
2023-03-12 16:32:50 +0900

    Configured prisma service for use with other modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // フロントエンドと Cookie ベースで通信するための Cors 設定
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  // フロントエンドからの Cookie を解析
+  app.use(cookieParser());
   await app.listen(3001);
 }
 bootstrap();
